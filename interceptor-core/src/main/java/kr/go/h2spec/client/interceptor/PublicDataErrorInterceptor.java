ce015ed71180e3db51924caf7fb481ad6d6d81b2
bbabang7709 <bbangeum1588@naver.com>
2026-08-26 16:38:07 +0900

    feat(interceptor-core): support large WebFlux error responses

--- interceptor-core/src/main/java/kr/go/h2spec/client/interceptor/PublicDataErrorInterceptor.java
+++ interceptor-core/src/main/java/kr/go/h2spec/client/interceptor/PublicDataErrorInterceptor.java
@@ -9,7 +9,24 @@ import org.springframework.util.StreamUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
-/** RestTemplate에서 공공데이터 API의 HTTP 200 위장 에러를 감지하는 인터셉터. */
+/**
+ * 공공데이터포털 계열 API의 HTTP 200 위장 에러를 감지하는 RestTemplate 인터셉터.
+ * <p>
+ * 응답 바디의 resultCode(또는 유사 필드)가 성공 코드가 아니거나 알려진 에러
+ * 키워드를 포함하면 {@link PublicDataApiException}을 던집니다.
+ * <p>
+ * <b>주의:</b> InputStream은 한 번만 읽을 수 있으므로, 이 인터셉터를 사용하는
+ * RestTemplate의 ClientHttpRequestFactory는 반드시
+ * {@link org.springframework.http.client.BufferingClientHttpRequestFactory}로 감싸야
+ * 이후 메시지 컨버터가 응답 바디를 다시 읽을 수 있습니다.
+ *
+ * <pre>{@code
+ * RestTemplate restTemplate = new RestTemplate(
+ *     new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory())
+ * );
+ * restTemplate.getInterceptors().add(new PublicDataErrorInterceptor());
+ * }</pre>
+ */
 public class PublicDataErrorInterceptor implements ClientHttpRequestInterceptor {
     private final PublicDataErrorDetector detector;
 
